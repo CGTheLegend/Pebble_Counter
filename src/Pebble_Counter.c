653f5c8a37e9254b32346e41c802b39097fea8bd
Cristian Gonzalez <cfgonzal@ucsc.edu>
2015-08-21 02:39:32 -0700

    - Added function to convert int into a string     source: http://forums.getpebble.com/discussion/comment/29591/   - currently broken, think it's an issue with the first loop - Added logs for debugging.

--- src/Pebble_Counter.c
+++ src/Pebble_Counter.c
@@ -2,7 +2,42 @@
 
 static Window *window;
 static TextLayer *text_layer;
-static int *count;
+int *count;
+
+char *itoa(int num){
+  static char buff[20] = {};
+  int i = 0, temp_num = num, length = 0;
+  char *string = buff;
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Finished variable init.");
+
+  // count how many characters in the number
+  if(num >= 0) {
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "In IF loop.");
+
+    while(temp_num) {
+
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "In WHILE loop.");
+
+      temp_num /= 10;
+      length++;
+    }
+
+    // assign the number to the buffer starting at the end of the number
+    for(i = 0; i < length; i++) {
+
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "In FOR loop.");
+
+      buff[(length-1)-i] = '0' + (num % 10);
+      num /= 10;
+    }
+    buff[i] = '\0'; // can't forget the null byte to properly end our string
+  }else{
+    return "Unsupported Number";
+  }
+  return string;
+}
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   /* This button currently does nothing. */
@@ -10,10 +45,12 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   count++;
+  text_layer_set_text(text_layer, itoa(*count));
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   count--;
+  text_layer_set_text(text_layer, itoa(*count));
 }
 
 static void click_config_provider(void *context) {
@@ -25,11 +62,11 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  int count = 0;
-  char[] tempText = "0";
+  count = 0;
+
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, tempText);
+  text_layer_set_text(text_layer, "0");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
